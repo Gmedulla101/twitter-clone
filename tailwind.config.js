56f2f401339e8c02014f39736101fdf9108a50c5
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-04-27 23:06:32 +0100

    Finalised responsiveness and basic functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        ssm: '20rem',
+      },
+      screens: {
+        sssm: '400px',
+      },
+    },
   },
   plugins: [],
 };
