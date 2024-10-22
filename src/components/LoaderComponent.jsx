b4dbc95dd3bad725be2505c229343d376165002e
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-10-22 17:16:06 +0100

    Finalised user search

--- src/components/LoaderComponent.jsx
+++ src/components/LoaderComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const LoaderComponent = () => {
   return (
-    <div className="flex justify-center mt-64">
+    <div className="flex justify-center">
       <CSpinner className="text-blue-500 w-12 h-12 text-3xl" />
     </div>
   );
--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -1,9 +1,12 @@
+import { useState, useEffect } from 'react';
+
 import axios from 'axios';
 import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 
 //IMPORTING NEEDED DEPS
 import SideBar from './SideBar';
+import LoaderComponent from './LoaderComponent';
 
 //IMPORTING CONTEXT HOOK
 import { useGlobalContext } from '../context';
@@ -13,6 +16,38 @@ const socket = io.connect('http://localhost:5000');
 
 const Messaging = () => {
   const { user } = useGlobalContext();
+  const [userSearch, setUserSearch] = useState('');
+  const [userSearchResults, setUserSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState('');
+
+  const handleChange = (event) => {
+    setUserSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        if (!userSearch) {
+          setIsLoading(false);
+          return;
+        }
+
+        setIsLoading(true);
+        const data = await axios.get(
+          `http://localhost:5000/api/v1/users/getUsers?user=${userSearch}`
+        );
+
+        setUserSearchResults(data.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, [userSearch]);
 
   return (
     <>
@@ -34,17 +69,32 @@ const Messaging = () => {
             <input
               type="text"
               placeholder="Search for a username or full name"
+              onChange={handleChange}
+              value={userSearch}
               className="border-2 border-gray-400 block mx-auto w-full h-10 rounded-full px-3 pb-1 outline-none focus:border-blue-500"
             />
           </div>
-
-          <div className="p-2">
-            <div className="border-2 border-gray-400 flex items-center gap-2 p-2">
-              <span className="h-16 w-16 border-2 border-gray-400 p-2 rounded-full flex justify-center items-center">
-                Image
-              </span>
-              <h3 className="font-semibold ">Username</h3>
+          {isLoading ? (
+            <div className="mt-3">
+              <LoaderComponent />
             </div>
+          ) : (
+            ''
+          )}
+          <div className="p-2 flex flex-col gap-3">
+            {userSearchResults.map((result) => {
+              return (
+                <div
+                  key={result._id}
+                  className="border-2 border-gray-200 flex items-center gap-2 p-2 rounded-lg shadow"
+                >
+                  <span className="h-16 w-16 border-2 border-gray-400 p-2 rounded-full flex justify-center items-center">
+                    Image
+                  </span>
+                  <h3 className="font-semibold ">{result.username}</h3>
+                </div>
+              );
+            })}
           </div>
         </section>
       )}
