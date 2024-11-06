80b07da59bafcc230dad922fa80208589df7bffb
Gmedulla101 <edosegheosamagbe@gmail.com>
2024-11-06 14:45:16 +0100

    Finalised following and unfollowing functionaliy for frontend

--- src/components/LoaderComponent.jsx
+++ src/components/LoaderComponent.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const LoaderComponent = () => {
   return (
     <div className="flex justify-center">
-      <CSpinner className="text-blue-500 w-12 h-12 text-3xl" />
+      <CSpinner className="text-blue-500 w-12 h-12 text-3xl my-24" />
     </div>
   );
 };
--- src/pages/ProfileInfo.jsx
+++ src/pages/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 //IMPORTING HELPER COMPONENTS
 import SideBar from '../components/SideBar';
 import Tweet from '../components/Tweet';
+import LoaderComponent from '../components/LoaderComponent';
 
 //IMPORTING DEPS
 import axios from 'axios';
@@ -12,10 +13,12 @@ const ProfileInfo = () => {
   const { username } = useParams();
   const [userData, setUserData] = useState();
   const [userTweets, setUserTweets] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const getUser = async () => {
       try {
+        setIsLoading(true);
         const data = await axios.get(
           `https://twitter-backend-s1nc.onrender.com/api/v1/users/getUser/${username}`
         );
@@ -26,8 +29,10 @@ const ProfileInfo = () => {
         setUserTweets(userTweetData.data.data);
 
         setUserData(data.data.data[0]);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
+        setIsLoading(false);
       }
     };
 
@@ -67,6 +72,31 @@ const ProfileInfo = () => {
         }
       );
       console.log('Followed');
+      location.reload();
+    } catch (error) {
+      console.log(error.response.data.msg);
+    }
+  };
+
+  const unfollowUser = async () => {
+    try {
+      const storedToken = localStorage.getItem('userToken');
+      if (!storedToken) {
+        throw new Error('You must sign in to make a post!');
+      }
+
+      const token = JSON.parse(storedToken);
+      await axios.patch(
+        `http://localhost:5000/api/v1/users/unfollowUser/${username}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log('unfollowed');
+      location.reload();
     } catch (error) {
       console.log(error.response.data.msg);
     }
@@ -75,64 +105,77 @@ const ProfileInfo = () => {
   return (
     <>
       <SideBar />
-      <section className="ml-12 mb-12 bg-white md:ml-64">
-        <section className=" h-full">
-          {/* MAIN PROFILE BODY */}
-          <div className="h-60 bg-slate-200 flex justify-center gap-4 text-2xl font-bold relative">
-            <img src={userData?.coverImage} alt="" className="w-full h-full" />
-          </div>
-
-          <div className="bg-white border-4 border-blue-400 w-20 h-20 p-1 rounded-full relative top-[-40px] ml-6 overflow-hidden">
-            <img src={userData?.profileImage} alt="" className="rounded-full" />
-          </div>
-        </section>
-
-        <section className="ml-3 flex items-center justify-between">
-          <div>
-            <h4 className="font-bold text-2xl ">
-              {userData?.firstName} {userData?.lastName} {userData?.otherNames}
-            </h4>
-            <p className="text-slate-500"> @{userData?.username} </p>
-            <p className="mt-2"> {userData?.bio} </p>
-            <div className="flex gap-2">
-              <p className="font-semibold">
-                {userData?.followers.length} followers
-              </p>
-              <p className="font-semibold">
-                {userData?.following.length} following
-              </p>
+      {isLoading ? (
+        <LoaderComponent />
+      ) : (
+        <section className="ml-12 mb-12 bg-white md:ml-64">
+          <section className=" h-full">
+            {/* MAIN PROFILE BODY */}
+            <div className="h-60 bg-slate-200 flex justify-center gap-4 text-2xl font-bold relative">
+              <img
+                src={userData?.coverImage}
+                alt=""
+                className="w-full h-full"
+              />
             </div>
-          </div>
 
-          <div>
-            {userData?.followers.includes(
-              JSON.parse(localStorage.getItem('userId'))
-            ) ? (
-              <button
-                onClick={followUser}
-                className="py-2 px-8 bg-blue-500 rounded-full text-white font-semibold "
-              >
-                unfollow
-              </button>
-            ) : (
-              <button
-                onClick={followUser}
-                className="py-2 px-10 bg-blue-500 rounded-full text-white font-semibold "
-              >
-                Follow
-              </button>
-            )}
-          </div>
-        </section>
+            <div className="bg-white border-4 border-blue-400 w-20 h-20 p-1 rounded-full relative top-[-40px] ml-6 overflow-hidden">
+              <img
+                src={userData?.profileImage}
+                alt=""
+                className="rounded-full"
+              />
+            </div>
+          </section>
 
-        <section className="mt-10">
-          <h1 className="text-xl text-center font-bold mb-3 text-blue-500">
-            {' '}
-            Your Tweets{' '}
-          </h1>
-          <div>{tweetsEl}</div>
+          <section className="ml-3 flex items-center justify-between p-2">
+            <div>
+              <h4 className="font-bold text-2xl ">
+                {userData?.firstName} {userData?.lastName}{' '}
+                {userData?.otherNames}
+              </h4>
+              <p className="text-slate-500"> @{userData?.username} </p>
+              <p className="mt-2"> {userData?.bio} </p>
+              <div className="flex gap-2">
+                <p className="font-semibold">
+                  {userData?.followers.length} followers
+                </p>
+                <p className="font-semibold">
+                  {userData?.following.length} following
+                </p>
+              </div>
+            </div>
+
+            <div>
+              {userData?.followers.includes(
+                JSON.parse(localStorage.getItem('userId'))
+              ) ? (
+                <button
+                  onClick={unfollowUser}
+                  className="py-2 px-8 bg-blue-500 rounded-full text-white font-semibold "
+                >
+                  unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={followUser}
+                  className="py-2 px-10 bg-blue-500 rounded-full text-white font-semibold "
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </section>
+
+          <section className="mt-10">
+            <h1 className="text-xl text-center font-bold mb-3 text-blue-500">
+              {' '}
+              Your Tweets{' '}
+            </h1>
+            <div>{tweetsEl}</div>
+          </section>
         </section>
-      </section>
+      )}
     </>
   );
 };
